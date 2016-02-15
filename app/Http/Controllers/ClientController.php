3dc452de37ab2e289aa40f52406266fb72e356e9
Silvio Barretto <silvioaabarretto@gmail.com>
2016-02-15 15:29:31 -0200

    Primeiro commit do projeto

---
+++ app/Http/Controllers/ClientController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Client;
+use Illuminate\Http\Request;
+
+use CodeProject\Http\Requests;
+use CodeProject\Http\Controllers\Controller;
+
+class ClientController extends Controller
+{
+    public function index()
+    {
+        return \CodeProject\Client::all();
+    }
+
+    public function store(Request $request)
+    {
+        return Client::create($request->all());
+    }
+
+    public function show($id)
+    {
+       return Client::find($id);
+    }
+
+    public function destroy($id)
+    {
+        Client::find($id)->delete();
+    }
+
+    public function update(Request $request, $id)
+    {
+        return Client::find($id)->update($request->all());
+    }
+}
