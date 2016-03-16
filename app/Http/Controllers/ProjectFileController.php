6c60604a6b174b37165bce6e5d629a067d9a62cc
Silvio Barretto <silvioaabarretto@gmail.com>
2016-03-16 13:08:01 -0300

    Presenters e Transformers

---
+++ app/Http/Controllers/ProjectFileController.php
@@ -0,0 +1,122 @@
+<?php
+
+namespace CodeProject\Http\Controllers;
+
+use CodeProject\Repositories\ProjectRepository;
+use CodeProject\Services\ProjectService;
+use Illuminate\Http\Request;
+use LucaDegasperi\OAuth2Server\Facades\Authorizer;
+
+class ProjectFileController extends Controller
+{
+    /**
+     * @var ProjectRepository
+     */
+    private $repository;
+
+    /**
+     * @var ProjectService
+     */
+    private $service;
+
+
+    /**
+     * ProjectController constructor.
+     * @param ProjectRepository $repository
+     * @param ProjectService $service
+     */
+    public function __construct(ProjectRepository $repository, ProjectService $service)
+    {
+        $this->repository = $repository;
+        $this->service = $service;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function index()
+    {
+        return $this->repository->findWhere(['owner_id' => Authorizer::getResourceOwnerId()]);
+
+        //return $this->repository->all();
+    }
+
+    /**
+     * @param Request $request
+     * @return mixed
+     */
+    public function store(Request $request)
+    {
+        $file = $request->file('file');
+        $extension = $file->getClientOriginalExtension();
+
+        $data['file'] = $file;
+        $data['extension'] = $extension;
+        $data['name'] = $request->name;
+        $data['project_id'] = $request->project_id;
+        $data['description'] = $request->description;
+
+        $this->service->createFile($data);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function show($id)
+    {
+        if($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+        return $this->repository->find($id);
+    }
+
+    /**
+     * @param $id
+     * @return int
+     */
+    public function destroy($id)
+    {
+        if($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+        return $this->repository->delete($id);
+    }
+
+    /**
+     * @param Request $request
+     * @param $id
+     * @return mixed
+     */
+    public function update(Request $request, $id)
+    {
+        if($this->checkProjectPermissions($id) == false) {
+            return ['error' => 'Access Forbidden'];
+        }
+        return $this->service->update($request->all(), $id);
+    }
+
+    /**
+     * @param $projectId
+     * @return array
+     */
+    private function checkProjectOwner($projectId)
+    {
+        $userId = Authorizer::getResourceOwnerId();
+        return $this->repository->isOwner($projectId, $userId);
+    }
+
+    private function checkProjectMember($projectId)
+    {
+        $userId = Authorizer::getResourceOwnerId();
+        return $this->repository->hasMember($projectId, $userId);
+    }
+
+    private function checkProjectPermissions($projectId)
+    {
+        if($this->checkProjectOwner($projectId) or $this->checkProjectMember($projectId)) {
+            return true;
+        }
+        return false;
+    }
+}
--- app/Services/ProjectService.php
+++ app/Services/ProjectService.php
@@ -13,6 +13,10 @@ use CodeProject\Repositories\ProjectRepository;
 use CodeProject\Validators\ProjectValidator;
 use Prettus\Validator\Exceptions\ValidatorException;
 
+use Illuminate\Contracts\Filesystem\Factory as Storage;
+
+use Illuminate\Filesystem\Filesystem;
+
 class ProjectService
 {
     /**
@@ -24,11 +28,21 @@ class ProjectService
      * @var ProjectValidator
      */
     protected $validator;
+    /**
+     * @var Filesystem
+     */
+    private $filesystem;
+    /**
+     * @var Storage
+     */
+    private $storage;
 
-    public function __construct(ProjectRepository $repository, ProjectValidator $validator)
+    public function __construct(ProjectRepository $repository, ProjectValidator $validator, Filesystem $filesystem, Storage $storage)
     {
         $this->repository = $repository;
         $this->validator = $validator;
+        $this->filesystem = $filesystem;
+        $this->storage = $storage;
     }
 
     public function create(array $data)
@@ -58,5 +72,13 @@ class ProjectService
                 'message' => $e->getMessageBag()
             ];
         }
+    }
+
+    public function createFile(array $data)
+    {
+        $project = $this->repository->skipPresenter()->find($data['project_id']);
+        $projectFile = $project->files()->create($data);
+
+        $this->storage->put($projectFile->id . "." . $data['extension'], $this->filesystem->get($data['file']));
     }
 }
